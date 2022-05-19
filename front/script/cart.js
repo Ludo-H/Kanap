139e91a946a00cda7ac46779e9032324afcc073d
Ludo Hund <Ludovic_hund@hotmail.fr>
2022-05-19 19:26:24 +0200

    refonte de contenu product et cart, ajout de fonctions pour gérer le panier, affichage du panier sans utiliser innerhtml, ajout calcul somme produit et prix, ajout evenement bouton supprimer, commentaire encore dans cart avec toutes les fonctions mais pas utiles

--- front/script/cart.js
+++ front/script/cart.js
@@ -1,68 +1,344 @@
-//-------------
+// ------------
 // VARIABLES
-//-------------
-
-// Variables dans lesquelles on va ajouter du contenu
-const sectionItems  = document.getElementById("cart__items");
-const totalQuantity = document.getElementById("totalQuantity");
-const totalPrice    = document.getElementById("totalPrice");
-const cartOrder     = document.querySelector(".cart__order");
-const inputQuantity = document.querySelectorAll(".itemQuantity");
-
-// On récupère le contenu du panier avec la clé "produit" et on le stocke dans cette variable
-let addProduct = JSON.parse(localStorage.getItem("product"));
-console.log(addProduct);
+// ------------
 
 
-//-------------
-// FONCTIONS
-//-------------
+//*******************************************************************
+// Pour afficher ou non le formulaire
+let formDiv = document.querySelector(".cart__order");
+let localStorageContains = JSON.parse(localStorage.getItem("basket"));
+console.log(localStorageContains);
 
-// Fonction pour afficher le contenu du panier
-async function cartDisplay() {
 
-    // si addproduct a du contenu, alors on l'attend 
-    if(addProduct){
-        await addProduct;
+if(localStorageContains == null || localStorageContains.length == 0){
+    formDiv.style.display = "none";
+}
+//*******************************************************************
 
-    // Puis on incrémente la section voulu de chaque produit grace à la méthode .map 
-    sectionItems.innerHTML = addProduct.map((product) =>
-        `
-        <article class="cart__item" data-id="${product._id}" data-color="${product.color}">
-            <div class="cart__item__img">
-                <img src="${product.imageUrl}" alt="${product.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${product.color}</p>
-                    <p>${(product.price)*(product.quantity)} €</p>
-                </div>
-                <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                        <p>Qté : ${product.quantity}</p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                        <p class="deleteItem">Supprimer</p>
-                    </div>
-                </div>
-            </div>
-        </article>
-        `
-        
-    );
 
-    // si addproduct n'a pas de contenu, alors on masque le formulaire de commande pour ne pas gérer des commandes vide plus tard
-    }else{
-        cartOrder.style.display = "none";
-    }
+
+//*******************************************************************
+// Pour afficher le contenu du panier
+let section = document.getElementById("cart__items");
+
+// On ne veut pas jouer forEach dans le vide
+function basketDisplay() {
+    if(localStorageContains != null){
+        localStorageContains.forEach(product => {
+        // Article qui contient tout
+        let article = document.createElement("article");
+        article.classList.add("cart__item");
+        article.setAttribute("data-id", `${product.id}`);
+        article.setAttribute("data-color", `${product.color}`);
+        section.appendChild(article);
+    
+        // La div et son image
+        let divImg = document.createElement("div");
+        divImg.classList.add("cart__item__img");
+        article.appendChild(divImg);
+        let img = document.createElement("img");
+        img.setAttribute("src", `${product.dataProduct.imageUrl}`);
+        img.setAttribute("alt", `${product.dataProduct.altTxt}`);
+        divImg.appendChild(img);
+    
+        // La div qui contient tous les renseignements
+        let divAllTxt = document.createElement("div");
+        divAllTxt.classList.add("cart__item__content");
+        article.appendChild(divAllTxt);
+    
+        // La div qui contient nom, couleur, prix
+        let divDescription = document.createElement("div");
+        divDescription.classList.add("cart__item__content__description");
+        divAllTxt.appendChild(divDescription);
+    
+        // Nom du produit
+        let nameProduct = document.createElement("h2");
+        nameProduct.textContent = `${product.dataProduct.name}`;
+        divDescription.appendChild(nameProduct);
+    
+        // Couleur du produit
+        let colorProduct = document.createElement("p");
+        colorProduct.textContent = `${product.color}`;
+        divDescription.appendChild(colorProduct);
+    
+        // Prix du produit
+        let priceProduct = document.createElement("p");
+        priceProduct.textContent = `${product.dataProduct.price * product.quantity} €`;
+        divDescription.appendChild(priceProduct);
+    
+        // La div qui contient bouttons et input
+        let divSettings = document.createElement("div");
+        divSettings.classList.add("cart__item__content__settings");
+        divAllTxt.appendChild(divSettings);
+    
+        // La div qui contient input quantité
+        let divQuantity = document.createElement("div");
+        divQuantity.classList.add("cart__item__content__settings__quantity");
+        divSettings.appendChild(divQuantity);
+    
+        // Info de la quantité
+        let paraQuantity = document.createElement("p");
+        paraQuantity.textContent = `Qté : ${product.quantity}`;
+        divQuantity.appendChild(paraQuantity);
+    
+        // Input de la quantité
+        let inputQuantity = document.createElement("input");
+        inputQuantity.setAttribute("type", "number");
+        inputQuantity.setAttribute("name", "itemQuantity");
+        inputQuantity.setAttribute("min", "1");
+        inputQuantity.setAttribute("max", "100");
+        inputQuantity.setAttribute("value", `${product.quantity}`);
+        inputQuantity.classList.add("itemQuantity");
+        divQuantity.appendChild(inputQuantity);
+    
+        // La div du boutton supprimer
+        let divDelete = document.createElement("div");
+        divDelete.setAttribute("id", `${product.id}`);
+        divDelete.setAttribute("data-color", `${product.color}`);
+        divDelete.classList.add("cart__item__content__settings__delete");
+        divSettings.appendChild(divDelete);
+    
+        // Le para supprimer
+        let paraDelete = document.createElement("p");
+        paraDelete.classList.add("deleteItem");
+        paraDelete.textContent = "Supprimer";
+        divDelete.appendChild(paraDelete);
+    });
+    };
 };
-cartDisplay();
+basketDisplay();
+//*******************************************************************
 
 
 
 
-//-------------
+//*******************************************************************
+let totalProducts = document.getElementById("totalQuantity");
+let totalPrice = document.getElementById("totalPrice");
+
+totalProducts.textContent = getNumberProduct();
+totalPrice.textContent = getTotalPrice();
+//*******************************************************************
+
+
+
+
+//------------
+// FONCTIONS
+//------------
+
+
+//*******************************************************************
+function getNumberProduct() {
+    if(localStorageContains != null){
+    let number = 0;
+    for (let product of localStorageContains){
+        number += product.quantity;
+    }
+    return number;
+}
+}
+//*******************************************************************
+
+
+
+
+//*******************************************************************
+function getTotalPrice() {
+    if(localStorageContains != null){
+    let total = 0;
+    for (let product of localStorageContains){
+        total += (product.dataProduct.price * product.quantity);
+    }
+    return total;
+}
+}
+//*******************************************************************
+
+
+
+
+//------------
 // EVENEMENTS
-//-------------
+//------------
+let divDelete = document.querySelectorAll(".cart__item__content__settings__delete");
+console.log(divDelete);
+
+for(let i = 0; i < divDelete.length; i++){
+    divDelete[i].addEventListener("click", ()=>{
+        console.log(divDelete[i].dataset.color);
+        localStorageContains = localStorageContains.filter(element => element.id != divDelete[i].id || element.color != divDelete[i].dataset.color);
+        console.log(localStorageContains);
+        localStorage.setItem("basket", JSON.stringify(localStorageContains));
+        location.reload();
+    });
+    
+};
+
+
+// for (let i = 0; i < localStorageContains.length; i++){
+
+// }
+
+// divDelete.forEach(button => {
+//     button.addEventListener("click", ()=>{
+//         localStorage.removeItem(button.id)
+//     });
+// });
+
+
+// function removeItemOfLS(button) {
+//     localStorageContains = localStorageContains.filter(product => product.id =! button.id);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// // Pour afficher ou non le formulaire
+// let formDiv = document.querySelector(".cart__order");
+
+// // Envoyer du contenu dans le LS
+// function saveBasket(basket) {
+//     localStorage.setItem("basket", JSON.stringify(basket));
+// }
+
+// // Récupérer le contenu du LS
+// function getBasket() {
+//     let basket = localStorage.getItem("basket");
+//     if(basket == null){
+//         return [];
+//     }else{
+//         return JSON.parse(basket)
+//     }
+// }
+
+// // Permet de retirer le produit voulu (en parametre)
+// function removeFromBasket(product) {
+//     let basket = getBasket();
+//     basket = basket.filter(basketProduct => basketProduct.id != product.id);
+//     saveBasket(basket);
+// }
+
+// // Pour afficher le contenu du panier
+// let section = document.getElementById("cart__items");
+
+// // On ne veut pas jouer forEach dans le vide
+// function basketDisplay() {
+//         let basket = getBasket();
+//         basket.forEach(product => {
+//         // Article qui contient tout
+//         let article = document.createElement("article");
+//         article.classList.add("cart__item");
+//         article.setAttribute("data-id", `${product.id}`);
+//         article.setAttribute("data-color", `${product.color}`);
+//         section.appendChild(article);
+    
+//         // La div et son image
+//         let divImg = document.createElement("div");
+//         divImg.classList.add("cart__item__img");
+//         article.appendChild(divImg);
+//         let img = document.createElement("img");
+//         img.setAttribute("src", `${product.dataProduct.imageUrl}`);
+//         img.setAttribute("alt", `${product.dataProduct.altTxt}`);
+//         divImg.appendChild(img);
+    
+//         // La div qui contient tous les renseignements
+//         let divAllTxt = document.createElement("div");
+//         divAllTxt.classList.add("cart__item__content");
+//         article.appendChild(divAllTxt);
+    
+//         // La div qui contient nom, couleur, prix
+//         let divDescription = document.createElement("div");
+//         divDescription.classList.add("cart__item__content__description");
+//         divAllTxt.appendChild(divDescription);
+    
+//         // Nom du produit
+//         let nameProduct = document.createElement("h2");
+//         nameProduct.textContent = `${product.dataProduct.name}`;
+//         divDescription.appendChild(nameProduct);
+    
+//         // Couleur du produit
+//         let colorProduct = document.createElement("p");
+//         colorProduct.textContent = `${product.color}`;
+//         divDescription.appendChild(colorProduct);
+    
+//         // Prix du produit
+//         let priceProduct = document.createElement("p");
+//         priceProduct.textContent = `${product.dataProduct.price * product.quantity} €`;
+//         divDescription.appendChild(priceProduct);
+    
+//         // La div qui contient bouttons et input
+//         let divSettings = document.createElement("div");
+//         divSettings.classList.add("cart__item__content__settings");
+//         divAllTxt.appendChild(divSettings);
+    
+//         // La div qui contient input quantité
+//         let divQuantity = document.createElement("div");
+//         divQuantity.classList.add("cart__item__content__settings__quantity");
+//         divSettings.appendChild(divQuantity);
+    
+//         // Info de la quantité
+//         let paraQuantity = document.createElement("p");
+//         paraQuantity.textContent = `Qté : ${product.quantity}`;
+//         divQuantity.appendChild(paraQuantity);
+    
+//         // Input de la quantité
+//         let inputQuantity = document.createElement("input");
+//         inputQuantity.setAttribute("type", "number");
+//         inputQuantity.setAttribute("name", "itemQuantity");
+//         inputQuantity.setAttribute("min", "1");
+//         inputQuantity.setAttribute("max", "100");
+//         inputQuantity.setAttribute("value", `${product.quantity}`);
+//         inputQuantity.classList.add("itemQuantity");
+//         divQuantity.appendChild(inputQuantity);
+    
+//         // La div du boutton supprimer
+//         let divDelete = document.createElement("div");
+//         divDelete.classList.add("cart__item__content__settings__delete");
+//         divSettings.appendChild(divDelete);
+    
+//         // Le para supprimer
+//         let paraDelete = document.createElement("p");
+//         paraDelete.classList.add("deleteItem");
+//         paraDelete.setAttribute("id", `${product.id}`);
+//         paraDelete.setAttribute("data-color", `${product.color}`);
+//         paraDelete.textContent = "Supprimer";
+//         divDelete.appendChild(paraDelete);
+//     });
+//     };
+// basketDisplay();
+
+// let totalProducts = document.getElementById("totalQuantity");
+// let totalPrice = document.getElementById("totalPrice");
+
+// totalProducts.textContent = getNumberProduct();
+// totalPrice.textContent = getTotalPrice();
+
+// function getNumberProduct() {
+//     let basket = getBasket();
+//     let number = 0;
+//     for (let product of basket){
+//         number += product.quantity;
+//     }
+//     return number;
+// }
+
+// function getTotalPrice() {
+//     let basket = getBasket();
+//     let total = 0;
+//     for (let product of basket){
+//         total += (product.dataProduct.price * product.quantity);
+//     }
+//     return total;
+// }
+
+// let deleteItem = document.querySelectorAll(".deleteItem");
+// console.log(deleteItem);
--- front/script/product.js
+++ front/script/product.js
@@ -13,8 +13,8 @@ const description = document.getElementById("description");
 const colors = document.getElementById("colors");
 const addToCart = document.getElementById("addToCart");
 
-// Tableau qui va contenir les produits du localStorage
-let dataProduct = [];
+// Tableau qui va contenir les data du canapé cliqué
+let dataProduct;
 
 
 //-------------
@@ -26,7 +26,7 @@ async function fetchProduct() {
     await fetch(`http://localhost:3000/api/products/${productId}`)
         .then((res) => res.json())
         .then((data) => dataProduct = data)
-    console.log(dataProduct);
+    // console.log(dataProduct);
     productDisplay();
 };
 fetchProduct();
@@ -38,7 +38,7 @@ function productDisplay() {
     price.textContent = `${dataProduct.price} `;
     description.textContent = `${dataProduct.description}`;
 
-    // ForEach pour créer une balise option en fonction du nombre de couleurs u produit de la page
+    // ForEach pour créer une balise option en fonction du nombre de couleurs du produit de la page
     dataProduct.colors.forEach(color => {
         let baliseOption = document.createElement("option");
         baliseOption.textContent = `${color}`;
@@ -48,47 +48,40 @@ function productDisplay() {
 };
 
 
+function saveBasket(basket) {
+    localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+function getBasket() {
+    let basket = localStorage.getItem("basket");
+    if(basket == null){
+        return [];
+    }else{
+        return JSON.parse(basket);
+    }
+}
+
+function addBasket(product) {
+    let basket = getBasket();
+    let foundProduct = basket.find(basketProduct => basketProduct.id == product.id && basketProduct.color == product.color);
+    if(foundProduct != undefined){
+        foundProduct.quantity += product.quantity;
+        alert("Produit ajouté au panier")
+    }else if (product.color != "" && product.quantity > 0){
+        basket.push(product);
+        alert("Produit ajouté au panier")
+    }
+    saveBasket(basket);
+}
+
 //-------------
 // EVENEMENTS
 //-------------
 
 // Click sur le boutton ajouter au panier
 addToCart.addEventListener("click", ()=>{
+    let newProduct = {dataProduct, color: colors.value, quantity: Number(quantity.value), id: dataProduct._id}
 
-    // Cette variable contient la valeur de la clé "product" du localstorage (grace a getItem), la méthode parse construit l'objet à partir de la chaine de product
-    let tabProduct = JSON.parse(localStorage.getItem("product"));
- 
-    // On créé un objet qui reprend tout de dataProduct et on y incrémente la couleur choisi ainsi que la quantité 
-    const addColorQuantity = Object.assign({}, dataProduct, {
-        color: colors.value,
-        // Pour ne pas melanger chaine et nombre, methode number
-        quantity: Number(quantity.value)
-    });
-
-    // si le tabproduct est vide (donc pas de LS) et que la balise option couleur est différente de vide et que la balise option quantité est différente de 0, alors on défini tabproduct en tableau, on y incrémente le new tableau avec valeurs en +, on créé une clé "product" en y mettant en valeur chaine le tabproduct, puis on construit l'objet avec parse. On a notre LS. (ces conditions permettent de ne pas envoyer un panier avec des quantité 0 et sans couleur)
-    if(tabProduct == null && colors.value != "" && quantity.value != 0 ){
-        tabProduct = [];
-        tabProduct.push(addColorQuantity);
-        localStorage.setItem("product", JSON.stringify(tabProduct));
-        tabProduct = JSON.parse(localStorage.getItem("product"));
-
-    // sinon si tabproduct a du contenu, 
-    }else if(tabProduct != null){
-
-        // on fait parcourir une boucle dans ses objets pour voir si l'id de i est égal à l'id du produit sous nos yeux et la color de i correspond à la couleur sous nos yeux, alors on incrémente la quantité de cet objet i par la valeur de la quantité sous nos yeux. On ajoute dans le LS existant la valeur grace a return qui ne va pas continuer sur l'autre boucle
-        for (let i = 0; i < tabProduct.length; i++){
-            if(tabProduct[i]._id == dataProduct._id && tabProduct[i].color == colors.value){
-                return (tabProduct[i].quantity += Number(quantity.value),
-                localStorage.setItem("product", JSON.stringify(tabProduct)));
-            };
-        };
-
-        // on refait parcourir une boucle si la précédente n'est pas retournée, si l'id de i coorespond à l'id sous nos yeux ET la couleur de i est différente de la couleur sous nos yeux ET que celle si n'est pas vide, OU l'id de i est différent de l'id sous nos yeux ET que la couleur sous nos yeux n'est pas vide ET que la quantité sous nos yeux nest pas 0, alors on y incrémente le new tableau avec valeurs en +, on créé une clé "product" en y mettant en valeur chaine le tabproduct, puis on construit l'objet avec parse. On a notre LS
-        for (let i = 0; i < tabProduct.length; i++){
-            if((tabProduct[i]._id == dataProduct._id && tabProduct[i].color != colors.value && colors.value != "") || (tabProduct[i]._id != dataProduct._id && colors.value != "" && quantity.value != 0)){
-                return (tabProduct.push(addColorQuantity),
-                localStorage.setItem("product", JSON.stringify(tabProduct)), tabProduct = JSON.parse(localStorage.getItem("product")));
-            };
-        };
-    };
+    addBasket(newProduct);
+   
 });
